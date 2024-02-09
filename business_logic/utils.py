bef04127aebba535cef59afd3410640c3788d2a5
Joshua Ajayi <joshuaajayi101@gmail.com>
2024-02-09 15:26:08 +0100

    fixes

--- business_logic/utils.py
+++ business_logic/utils.py
@@ -1,13 +1,13 @@
 import boto3
 
 
-def aws_sesion():
+def aws_session():
     session = boto3.Session(
         # aws_access_key_id=Variable.get("access_key"),
         # aws_secret_access_key=Variable.get("secret_access_key"),
         
-        aws_access_key_id='lxbXJSDhFboeJSxGh3pfW2YOBYfBZIj/VMkWTIQ2',
-        aws_secret_access_key='AKIASWZHRVTXOJDM5KZW',
+        aws_access_key_id='',
+        aws_secret_access_key='',
         region_name="eu-central-1"
     )
     return session
