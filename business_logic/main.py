d77e8d5cab8dccaa3d86ec1833dd130d1fb1efc6
joachimuche <joachim.uche@gmail.com>
2024-02-12 18:24:35 +0100

    update business logic

--- business_logic/main.py
+++ business_logic/main.py
@@ -1,24 +1,32 @@
 import pandas as pd
 from faker import Faker
 import awswrangler as wr
-from utils import aws_sesion
+from utils import aws_session
+from datetime import datetime
 
 
 def profile_data_generator(df_range: int):
     sample = Faker()
-
     dataframe = pd.DataFrame([sample.profile() for profile in range(df_range)])
 
     return dataframe
 
 
 def data_extract_to_s3():
-
     wr.s3.to_parquet(
                     df=profile_data_generator(200),
-                    path="s3://raw-data-regional/user_profile/",
-                    boto3_session=aws_sesion(),
+                    path=f"s3://raw-data-regional/regional_user_profile/",
+                    boto3_session=aws_session(),
                     mode="append",
-                    dataset=True
+                    dataset=True,
+                    database='testfaker',
+                    table='userprofile'
                     )
     return "Data successfully written to s3"
+
+# def extract_to_csv():
+#     data = profile_data_generator(200)
+#     data.to_csv(path_or_buf=f"user_profile{(datetime.now())}.csv", index=False)
+
+# data_extract_to_s3()
+data_extract_to_s3()
--- business_logic/utils.py
+++ business_logic/utils.py
@@ -1,13 +1,17 @@
 import boto3
+import boto3.session
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def aws_session():
-    session = boto3.Session(
+    session = boto3.session.Session(
         # aws_access_key_id=Variable.get("access_key"),
         # aws_secret_access_key=Variable.get("secret_access_key"),
         
-        aws_access_key_id='',
-        aws_secret_access_key='',
+        aws_access_key_id= os.getenv('access_id'),
+        aws_secret_access_key=os.getenv('secret_id'),
         region_name="eu-central-1"
     )
     return session
